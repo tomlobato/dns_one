9a9243503f6d2c5f523271bef5781cd371850fe5
Tom Lobato <tomlobato@gmail.com>
2018-03-28 14:50:15 -0300

    fix Util constant

--- lib/dns_one/util.rb
+++ lib/dns_one/util.rb
@@ -1,54 +1,56 @@
-module DnsOne; class Util; class << self
-
+module DnsOne; class Util
+    
   DOM_REGEX = /^[a-z0-9]+([\-\.][a-z0-9]+)*\.[a-z]{2,32}$/i
 
-  def die msg
-    Log.f msg
-    exit 1
-  end
+  class << self
+    def die msg
+      Log.f msg
+      exit 1
+    end
 
-  def run cmd
-      puts "Running #{cmd}..."
-      system cmd
-  end
+    def run cmd
+        puts "Running #{cmd}..."
+        system cmd
+    end
 
-  def has_systemd?
-      File.exist?(`which systemctl`.strip) && 
-      File.writable?('/lib/systemd/system')
-  end
+    def has_systemd?
+        File.exist?(`which systemctl`.strip) && 
+        File.writable?('/lib/systemd/system')
+    end
 
-  def ensure_sytemd
-      unless has_systemd?
-          STDERR.puts "Systemd not available. Aborting." 
-          exit 1
-      end
-  end
+    def ensure_sytemd
+        unless has_systemd?
+            STDERR.puts "Systemd not available. Aborting." 
+            exit 1
+        end
+    end
 
-  def match_root stat
-      stat.uid == 0 && stat.gid == 0
-  end
+    def match_root stat
+        stat.uid == 0 && stat.gid == 0
+    end
 
-  def last_mod constant
-    constant.to_s.split('::').last
-  end
+    def last_mod constant
+      constant.to_s.split('::').last
+    end
 
-  def log_result ip_address, domain_name, res_class, rcode, resp_log, from_cache
-    fields = []
+    def log_result ip_address, domain_name, res_class, rcode, resp_log, from_cache
+      fields = []
 
-    fields << domain_name
-    fields << Util.last_mod(res_class)
-    fields << rcode
-    fields << resp_log.map{ |rec|
-        Util.last_mod(rec.res_class) + 
-        ':' +
-        [rec.val].flatten.join(',')
-    }.join(';')
-    fields << ip_address
-    fields << (from_cache ? '1' : '0')
+      fields << domain_name
+      fields << Util.last_mod(res_class)
+      fields << rcode
+      fields << resp_log.map{ |rec|
+          Util.last_mod(rec.res_class) + 
+          ':' +
+          [rec.val].flatten.join(',')
+      }.join(';')
+      fields << ip_address
+      fields << (from_cache ? '1' : '0')
 
-    fields.map!{|v| v.blank? ? '-' : v}
+      fields.map!{|v| v.blank? ? '-' : v}
 
-    Log.i "result: #{ fields.join ' ' }"
-end
+      Log.i "result: #{ fields.join ' ' }"
+    end
+  end 
 
-end; end; end
+end; end
