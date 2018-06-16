6421ef8a86d39fb1276d017c7ec6a982d0ad0131
Tom Lobato <tomlobato@gmail.com>
2018-06-16 16:16:34 -0300

    add deep_dup

--- lib/dns_one/core_ext/hash.rb
+++ lib/dns_one/core_ext/hash.rb
@@ -2,4 +2,7 @@ class Hash
     def symbolize_keys
         each_with_object({}) { |(k, v), h| h[k.to_sym] = v.is_a?(Hash) ? v.symbolize_keys : v }
     end
+    def deep_dup
+        Marshal.load Marshal.dump(self)
+    end
 end
